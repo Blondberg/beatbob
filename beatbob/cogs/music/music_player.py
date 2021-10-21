c3d49ae31f59a7795045f3dfbe37dd65e74ad7ee
Blondberg <iamblondberg@gmail.com>
2021-10-21 14:47:22 +0200

    help

--- beatbob/cogs/music/music_player.py
+++ beatbob/cogs/music/music_player.py
@@ -79,9 +79,9 @@ class MusicPlayer:
             return
 
         async with ctx.typing():
-            players = await YTDLSource.from_url(url, loop=self.bot.loop)
-            for player in players:
-                await self.queue.put(player)
+            player = await YTDLSource.from_url(url, loop=self.bot.loop)
+
+            await self.queue.put(player)
             await ctx.send("Queued song: {} - [{}]".format(player.title, player.duration))
         return
 
